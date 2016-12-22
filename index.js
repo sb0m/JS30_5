a7fde16f0cb8c88e33ad3adc02711e5a873b68bc
subo <susann.bombor@zebware.com>
2016-12-22 11:25:21 +0100

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    let panels = document.querySelectorAll(".panel");
+    // Reference to the funtion: toggleOpen
+    // run the function: toggleOpen()
+    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+}
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    // here are 2 things transitioned, so you've to select one
+    // in Safari just flex, in the rest flex-grow
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
